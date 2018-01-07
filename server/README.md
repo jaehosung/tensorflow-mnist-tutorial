52b16afaaf7d0a7ae11e3ba12650a212fb6545a3
jaehossung@gmail.com <jaehossung@gmail.com>
2018-01-07 13:15:43 +0900

    Mnist using my own data

---
+++ server/README.md
@@ -0,0 +1,23 @@
+# How to Deploy a Keras Model to Production
+
+
+## Overview
+
+This is the code for [this](https://youtu.be/f6Bf3gl4hWY) video on Youtube by Siraj Raval. We're going to build a model that recognizes handwritten digit images (MNIST).  Developed using [TensorFlow](https://www.tensorflow.org/) and the super simple [Keras](http://keras.io/) Library. Wrapped into a Webapp using [Flask](http://flask.pocoo.org/) Micro Framework.
+
+## Dependencies
+
+```sudo pip install -r requirements.txt```
+
+## Usage
+
+Once dependencies are installed, just run this to see it in your browser. 
+
+```python app.py```
+
+That's it! It's serving a saved Keras model to you via Flask. 
+
+## Credits
+
+The credits for this code go to [moinudeen](https://github.com/moinudeen). I've merely created a wrapper to get people started.
+
---
+++ server/app.py
@@ -0,0 +1,86 @@
+#our web app framework!
+
+#you could also generate a skeleton from scratch via
+#http://flask-appbuilder.readthedocs.io/en/latest/installation.html
+
+#Generating HTML from within Python is not fun, and actually pretty cumbersome because you have to do the
+#HTML escaping on your own to keep the application secure. Because of that Flask configures the Jinja2 template engine 
+#for you automatically.
+#requests are objects that flask handles (get set post, etc)
+from flask import Flask, render_template,request
+#scientific computing library for saving, reading, and resizing images
+from scipy.misc import imsave, imread, imresize
+#for matrix math
+import numpy as np
+#for importing our keras model
+import keras.models
+#for regular expressions, saves time dealing with string data
+import re
+
+#system level operations (like loading files)
+import sys 
+#for reading operating system data
+import os
+#tell our app where our saved model is
+sys.path.append(os.path.abspath("./model"))
+from load import * 
+#initalize our flask app
+app = Flask(__name__)
+#global vars for easy reusability
+global model, graph
+#initialize these variables
+model, graph = init()
+
+#decoding an image from base64 into raw representation
+def convertImage(imgData1):
+	imgstr = re.search(r'base64,(.*)',imgData1).group(1)
+	#print(imgstr)
+	with open('output.png','wb') as output:
+		output.write(imgstr.decode('base64'))
+	
+
+@app.route('/')
+def index():
+	#initModel()
+	#render out pre-built HTML file right on the index page
+	return render_template("index.html")
+
+@app.route('/predict/',methods=['GET','POST'])
+def predict():
+	#whenever the predict method is called, we're going
+	#to input the user drawn character as an image into the model
+	#perform inference, and return the classification
+	#get the raw data format of the image
+	imgData = request.get_data()
+	#encode it into a suitable format
+	convertImage(imgData)
+	print "debug"
+	#read the image into memory
+	x = imread('output.png',mode='L')
+	#compute a bit-wise inversion so black becomes white and vice versa
+	x = np.invert(x)
+	#make it the right size
+	x = imresize(x,(28,28))
+	#imshow(x)
+	#convert to a 4D tensor to feed into our model
+	x = x.reshape(1,28,28,1)
+	print "debug2"
+	#in our computation graph
+	with graph.as_default():
+		#perform the prediction
+		out = model.predict(x)
+		print(out)
+		print(np.argmax(out,axis=1))
+		print "debug3"
+		#convert the response to a string
+		response = np.array_str(np.argmax(out,axis=1))
+		return response	
+	
+
+if __name__ == "__main__":
+	#decide what port to run the app in
+	port = int(os.environ.get('PORT', 5000))
+	#run the app locally on the givn port
+	app.run(host='0.0.0.0', port=port)
+	#optional if we want to run in debugging mode
+	#app.run(debug=True)
